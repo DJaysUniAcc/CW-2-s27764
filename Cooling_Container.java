d6126f4e12aa7218e7859765744c20c58d6a84eb
DJaysUniAcc <s27764@pjwstk.edu.pl>
2025-03-25 23:57:14 +0100

    Update

--- Cooling_Container.java
+++ Cooling_Container.java
@@ -1,8 +1,8 @@
-public class Liquid_Container extends Container {
+public class Cooling_Container extends Container {
     private boolean isHazardous;
 
-    public Liquid_Container(int own_mass, int carried_mass, int max_carried_mass, int height, int depth, boolean isHazardous) {
-        String serial_number = "KON-L-" + serial_number_count++;
+    public Cooling_Container(int own_mass, int carried_mass, int max_carried_mass, int height, int depth, boolean isHazardous) {
+        String serial_number = "KON-C-" + serial_number_count++;
         this.own_mass = own_mass;
         this.carried_mass = carried_mass;
         this.max_carried_mass = max_carried_mass;
